921c80ea29167338e3ab849012034bfe7e3b9d27
Mani_Dev <dhingramsp@Gmail.com>
2022-08-18 16:35:31 +0530

    browser stuff

---
+++ ModHelperData.cs
@@ -0,0 +1,12 @@
+﻿namespace BloonChipper
+{
+    public static class ModHelperData
+    {
+        public const string Version = "1.0.0";
+        public const string Name = "OP Towers";
+        public const string Description = "Memes";
+        public const string RepoOwner = "Mani-cwaf";
+        public const string RepoName = "OPTowers";
+        public const string DllName = "OPTowers.dll";
+    }
+}
